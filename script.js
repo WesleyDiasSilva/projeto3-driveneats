8dd4e7d34c9c31bdc23cc78cd4673072b17a262b
Wesley Dias <wesley.dias.dev@gmail.com>
2022-08-10 23:26:52 -0300

    Estou quase finalizando a parte obrigatória do projeto, preciso corrigir a parte do envio da mensagem pelo whatsapp

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ const arrayAlmoco = Array.from(itensAlmoço)
 const arrayBebida = Array.from(itensBebida)
 const arraySobremesa = Array.from(itenssobremesa)
 const botao = document.querySelector('.botao')
+const comida = ''
+const bebida = ''
+const sobremesa = ''
 
-const carrinho = []
+let carrinhoComida = []
+let carrinhoBebida = []
+let carrinhoSobremesa = []
 
 
 function selecionadoComida(){
@@ -16,9 +21,26 @@ function selecionadoComida(){
     }
   })
   this.classList.add('active')
-  carrinho.push('comida')
 
-  if(carrinho.includes('comida') && carrinho.includes('bebida') && carrinho.includes('sobremesa')){
+  //Até aqui eu só troquei a classe do css para modificar a aparencia do elemento
+
+  carrinhoComida.forEach(item => {
+    if(item.includes('comida')){
+      carrinhoComida = []
+    }
+  })
+  
+  carrinhoComida.push(this.childNodes[3].innerText,'comida')
+  var valorComida = this.childNodes[7].innerText
+  valorComida = valorComida.replace('R$','')
+  valorComida = valorComida.replace(',','.')
+  valorComida = Number(valorComida)
+  carrinhoComida.push(valorComida)
+  console.log(carrinhoComida)
+
+  //Aqui eu to colocando o produto selecionado num array
+
+  if(carrinhoComida[0] !== undefined && carrinhoBebida[0] !== undefined && carrinhoSobremesa[0] !== undefined){
     botao.innerHTML = 'Fechar pedido'
     botao.classList.remove('disable')
     botao.classList.add('botao-ativo')
@@ -32,9 +54,25 @@ function selecionadoBebida(){
     }
   })
   this.classList.add('active')
-  carrinho.push('bebida')
+  carrinhoBebida.push('bebida')
 
-  if(carrinho.includes('comida') && carrinho.includes('bebida') && carrinho.includes('sobremesa')){
+  carrinhoBebida.forEach(item => {
+    if(item.includes('bebida')){
+      carrinhoBebida = []
+    }
+  })
+  
+  carrinhoBebida.push(this.childNodes[3].innerText,'bebida')
+  let valorBebida = this.childNodes[7].innerText
+  valorBebida = valorBebida.replace('R$','')
+  valorBebida = valorBebida.replace(',','.')
+  valorBebida = Number(valorBebida)
+  carrinhoBebida.push(valorBebida)
+  console.log(carrinhoBebida)
+
+
+
+  if(carrinhoComida[0] !== undefined && carrinhoBebida[0] !== undefined && carrinhoSobremesa[0] !== undefined){
     botao.innerHTML = 'Fechar pedido'
     botao.classList.remove('disable')
     botao.classList.add('botao-ativo')
@@ -48,9 +86,24 @@ function selecionadoSobremesa(){
     }
   })
   this.classList.add('active')
-  carrinho.push('sobremesa')
+  carrinhoSobremesa.push('sobremesa')
 
-  if(carrinho.includes('comida') && carrinho.includes('bebida') && carrinho.includes('sobremesa')){
+  carrinhoSobremesa.forEach(item => {
+    if(item.includes('sobremesa')){
+      carrinhoSobremesa = []
+    }
+  })
+  
+  carrinhoSobremesa.push(this.childNodes[3].innerText, 'sobremesa')
+  let valorSobremesa = this.childNodes[7].innerText
+  valorSobremesa = valorSobremesa.replace('R$','')
+  valorSobremesa = valorSobremesa.replace(',','.')
+  valorSobremesa = Number(valorSobremesa)
+  carrinhoSobremesa.push(valorSobremesa)
+  console.log( carrinhoSobremesa)
+
+
+  if(carrinhoComida[0] !== undefined && carrinhoBebida[0] !== undefined && carrinhoSobremesa[0] !== undefined){
     botao.innerHTML = 'Fechar pedido'
     botao.classList.remove('disable')
     botao.classList.add('botao-ativo')
@@ -69,4 +122,19 @@ arrayBebida.forEach(item => {
   item.addEventListener('click', selecionadoBebida)
 })
 
-console.log(carrinho)
+const valorComida = carrinhoComida[2]
+const valorBebida = carrinhoBebida[2]
+const valorSobremesa = carrinhoSobremesa[2]
+const valorPedido = valorComida + valorBebida + valorSobremesa
+
+const linha1 = 'Olá, gostaria de fazer o pedido:'
+const linha2 = '- Prato: ' + carrinhoComida[0]
+const linha3 = '- Bebida:' + carrinhoBebida[0]
+const linha4 = '- Sobremesa:' + carrinhoSobremesa[0]
+const linha5 = 'Total: R$' + valorPedido
+
+const msg = linha1 + '\n' + linha2 + '\n' + linha3 + '\n' + linha4 + '\n' + linha5
+
+const url = encodeURIComponent(msg)
+
+botao.href = 'https://wa.me/5514996124147?text='+msg
